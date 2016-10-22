9e9f68b96def04c1de5dd4b0903034b23740ca79
Stephen Beirlaen <stephen.beirlaen@student.howest.be>
2016-10-22 18:32:10 +0200

    forgot RegisterFragment

---
+++ app/src/main/java/be/nmct/unitycard/fragments/RegisterFragment.java
@@ -0,0 +1,55 @@
+package be.nmct.unitycard.fragments;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import be.nmct.unitycard.R;
+import butterknife.ButterKnife;
+
+public class RegisterFragment extends Fragment {
+
+    private final String LOG_TAG = this.getClass().getSimpleName();
+    private RegisterFragmentListener mListener;
+
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+    public static RegisterFragment newInstance() {
+        return new RegisterFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    public interface RegisterFragmentListener {
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        // zeker zijn dat de container activity de callback geimplementeerd heeft
+        if (context instanceof RegisterFragmentListener) {
+            mListener = (RegisterFragmentListener)context;
+        } else {
+            throw new ClassCastException(context.toString() + "must implement " + RegisterFragmentListener.class.getSimpleName());
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+}
