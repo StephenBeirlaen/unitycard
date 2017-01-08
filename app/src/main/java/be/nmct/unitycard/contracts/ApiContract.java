b4b8430183b0444712a4924104bb70176ffc8d5a
Stephen Beirlaen <stephen.beirlaen@student.howest.be>
2017-01-08 01:20:18 +0100

    now using new geocoding api url

--- app/src/main/java/be/nmct/unitycard/contracts/ApiContract.java
+++ app/src/main/java/be/nmct/unitycard/contracts/ApiContract.java
@@ -6,7 +6,7 @@ package be.nmct.unitycard.contracts;
 
 public class ApiContract {
     public static final String API_URL = "https://unitycard.azurewebsites.net";
-    public static final String GOOGLE_GEOCODE_API_URL = "http://maps.google.com";
+    public static final String GOOGLE_GEOCODE_API_URL = "http://maps.googleapis.com";
     public static final String EXT_AUTH_PROVIDER_FACEBOOK = "Facebook";
     public static final String EXT_AUTH_PROVIDER_GOOGLE = "Google";
 }
